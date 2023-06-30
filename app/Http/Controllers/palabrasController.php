2b15dfb3279e79397140d5beaa3db02212f93c73
manuelmtz1864 <manuelmtz1864@gmail.com>
2023-06-30 13:08:39 -0600

    change

--- app/Http/Controllers/palabrasController.php
+++ app/Http/Controllers/palabrasController.php
@@ -14,20 +14,12 @@ class palabrasController extends Controller
     public function procesarPalabras(Request $request ){
 
         $texto = $request->palabrasText;
-
-        // Convertir el texto a minúsculas
         $texto = strtolower($texto);
 
-        // Eliminar los signos de puntuación
-        //$texto = preg_replace('/[^\w\s]/', '', $texto);
-
-        // Dividir el texto en palabras
         $palabras = explode(' ', $texto);
 
-        // Contar las repeticiones de cada palabra
         $contador = array_count_values($palabras);
 
-        // Devolver el resultado en formato JSON
         return response()->json($contador);
     }
 }
